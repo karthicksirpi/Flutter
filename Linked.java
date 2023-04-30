6012d3db21ef269ffa63ab61f5d812c71f046688
karthicksirpi <karthick.charan@datasirpi.com>
2023-04-30 10:52:22 +0530

    new1

---
+++ Linked.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+
+public class Linked {
+    public static void main(String[] args)
+    {
+        LinkedList<String> ll = new LinkedList<String>();
+        LinkedList<String> s= new LinkedList<String>();
+        s.add("a");
+        s.add("b");
+        s.add("c");
+        s.add("d");
+        s.add("e");
+        s.add("f");
+System.out.println(s);
+
+
+    }
+}
